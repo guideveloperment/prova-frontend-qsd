5a88db22bdb44bab450eaf8e3d11f31d1dacac72
Guilherme Viana <gaviana@rd.com.br>
2021-11-22 12:55:09 -0300

    terceiro commit provas Jonas

--- js/script.js
+++ js/script.js
@@ -5,10 +5,58 @@ var body = document.querySelector("body");
 
 
 btnSignin.addEventListener("click", function () {
-   body.className = "sign-in-js"; 
+    body.className = "sign-in-js";
 });
 
 btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
 })
+
+
+let iptCpf = document.getElementById("cpf");
+iptCpf.onblur = function () {
+    let regexCpf = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
+    let msgCpfError = document.querySelector(".msg-cpf.msg-error");
+    let msgCpfOK = document.querySelector(".msg-cpf.msg-success");
+
+    if (regexCpf.test(iptCpf.value)) {
+        msgCpfError.style.display = "none";
+        msgCpfOK.style.display = "block";
+    } else {
+        msgCpfOK.style.display = "none";
+        msgCpfError.style.display = "block";
+    }
+}
+
+
+
+function valida(senha) {
+    let regex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%*()_+^&}{:;?.])(?:([0-9a-zA-Z!@#$%;*(){}_+^&])(?!\1)){6,}$/;
+    if (regex.test(senha)) {
+        console.log(senha, '= válida');
+    } else {
+        console.log(senha, '= inválida');
+    }
+}
+
+['a@1', 'abc@123', 'aab@123'].forEach(s => valida(s));
+
+let iptEmail = document.getElementById("email");
+
+iptEmail.onblur = function() {
+    var regexMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    let msgEmail = document.querySelector(".msg-email");
+    let msgEmailOk = document.querySelector(".msg-email.ok");
+
+    if (regexMail.test(iptEmail.value)) {
+        msgEmail.style.display = "none";
+        msgEmailOk.style.display = "block";
+    } else {
+        msgEmailOk.style.display = "none";
+        msgEmail.style.display = "block";
+    }
+}
+
+
+
 
